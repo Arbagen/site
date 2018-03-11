aebfe1e37008127408c6bc9de4b645300dad86c1
Dante <a.bavyka@netpeak.net>
2018-03-11 11:13:05 +0200

    refactoring styles and js, add faq amd contact pages

--- public_html/src/Controller/PagesController.php
+++ public_html/src/Controller/PagesController.php
@@ -37,13 +37,13 @@ class PagesController extends Controller
         ]);
     }
     /**
-     * @Route("/contact", name="contact")
+     * @Route("/contact", name="front.contact")
      * @return \Symfony\Component\HttpFoundation\Response
      */
     public function contactsAction()
     {
         $title = 'Contact Us';
-        $map = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5491.9920703683465!2d30.686223790575646!3d46.508188856960885!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40c62e3756431ebd%3A0x506428d537752fec!2sShkodovaya+hora+St%2C+3%2C+Odesa%2C+Odessa+Oblast%2C+65000!5e0!3m2!1sen!2sua!4v1515956237291";
+        $map = "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d21966.961467585195!2d30.688882!3d46.51068!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40c62e3756431ebd%3A0x506428d537752fec!2sShkodovaya+hora+St%2C+3%2C+Odesa%2C+Odessa+Oblast%2C+65000!5e0!3m2!1sen!2sua!4v1520758370043";
     
         return $this->render('@site/pages/contacts.html.twig', [
           'title' => $title,
@@ -55,12 +55,24 @@ class PagesController extends Controller
      * @Route("/products", name="front.products")
      * @return Response
      */
-    public function productACtion()
+    public function productAction()
     {
         $title = 'Products';
         return $this->render('@site/pages/work-three-columns.html.twig',[
             'title' => $title,
         ]);
-
     }
+
+    /**
+     * @Route("/faq", name="front.faq")
+     * @return Response
+     */
+    public function faqAction()
+    {
+        $title = 'FAQ';
+        return $this->render('@site/pages/faq.html.twig',[
+            'title' => $title,
+        ]);
+    }
+
 }
